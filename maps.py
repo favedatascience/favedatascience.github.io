3492fc2e93545da5393910d49bb97d9fc6c1bbb9
Fares Hasan <farishassan2012@gmail.com>
2019-08-06 11:24:54 +0800

    company_name markers

--- maps.py
+++ maps.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import folium
-from folium.plugins import FastMarkerCluster, MeasureControl, DualMap
+from folium.plugins import FastMarkerCluster, MeasureControl, DualMap, MarkerCluster
 
-m_zero = pd.read_csv('/Users/aiyas/Downloads/QueryResults.csv')
-
+m_zero = pd.read_csv('QueryResultss.csv')
+#m_zero.fillna(0, inplace =True)
 o = folium.Map(location=[3, 102], zoom_start=2)
+mc = MarkerCluster()
 
-o.add_child(FastMarkerCluster(m_zero[['latitude', 'longitude']].values.tolist(),overlay=False))
-
+for row in m_zero.itertuples():
+    #folium.Marker([m_zero.iloc[i]['longitude'], m_zero.iloc[i]['latitude']], popup = m_zero.iloc[i]['company_name']).add_to(o)
+    #o.add_child(FastMarkerCluster(m_zero[['latitude', 'longitude']].values.tolist(),overlay=False))
+    mc.add_child(folium.Marker(location=[row.latitude,  row.longitude], popup=row.company_name))
+o.add_child(mc)
 o.save('index.html')
