51f672217bbeb380cacc414b8782db626941685c
Fares Hasan <farishassan2012@gmail.com>
2020-05-27 16:55:39 +0800

    latest update

--- maps.py
+++ maps.py
@@ -8,10 +8,14 @@ m_zero.dropna(inplace =True)
 o = folium.Map(location=[3, 102], zoom_start=2)
 mc = MarkerCluster()
 
+print(type(m_zero['address_latitude']))
+print(type(m_zero['address_longitude']))
+
 for row in m_zero.itertuples():
     #folium.Marker([m_zero.iloc[i]['longitude'], m_zero.iloc[i]['latitude']], popup = m_zero.iloc[i]['company_name']).add_to(o)
     #o.add_child(FastMarkerCluster(m_zero[['latitude', 'longitude']].values.tolist(),overlay=False))
     try:
+
         mc.add_child(folium.Marker(location=[row.address_latitude,  row.address_longitude], popup=row.company_name))
     except Exception as e:
         print(row.address_latitude,  row.address_longitude)
